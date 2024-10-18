99dd0aeb6a1e5390c56bfd8a15fe37f2b3dda838
iteknosys-solutions24 <iteknosys.solutions24@gmail.com>
2024-10-18 16:27:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  //console.log(`Hello Shashank you pressed me ` + count + ` times`);
 
   return (
     <>
@@ -17,19 +19,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>React Js Zero to Hero</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
